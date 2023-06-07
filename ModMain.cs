4bbf162eb07a54a9c9aa06ba76b12596b52f8819
Millipede406 <97822695+Millipede406@users.noreply.github.com>
2023-06-07 13:40:25 +0800

    HealthCurve patch

--- ModMain.cs
+++ ModMain.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using MelonLoader;
+using HarmonyLib;
+using PatchQuest;
 
 namespace MillimediaGames.NoHit
 {
     public class ModMain : MelonMod
     {
+        
+    }
 
+    [HarmonyPatch(typeof(Scaling))]
+    [HarmonyPatch(nameof(Scaling.HealthCurve))]
+    [HarmonyPatch(MethodType.Getter)]
+    public class HealthCurvePatch
+    {
+        public static void Postfix(ref float __result)
+        {
+            MelonLogger.Msg("HealthCurve get patched");
+            __result = 1f;
+        }
     }
 }
