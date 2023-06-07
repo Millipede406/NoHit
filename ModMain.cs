09bf28a7bd8d9fd2da01c3969a531d336af1d0c3
Millipede406 <97822695+Millipede406@users.noreply.github.com>
2023-06-07 11:47:25 +0800

    The to-do list

--- ModMain.cs
+++ ModMain.cs
@@ -3,6 +3,18 @@ using MelonLoader;
 
 namespace MillimediaGames.TheChallenge
 {
+    /* 
+     * The To-Do list
+     * 1. The lasso doesn't work
+     * 2. You only have 1 health (seems unfair, might remove)
+     * 3. You can't pick up ammo fruits
+     * 4. You can't capture pins (the pin press doesn't work at all)
+     * 5. All stats do nothing
+     * 6. All positive ailments have no effect
+     * 7. You can't enter dungeons
+     * 8. The other base camp entrances can't be opened
+    */ 
+
     public class ModMain : MelonMod
     {
 
