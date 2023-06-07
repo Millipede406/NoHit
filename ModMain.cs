12e814f46ed565d2d7779dcff316f281e361c343
Millipede406 <97822695+Millipede406@users.noreply.github.com>
2023-06-07 14:09:08 +0800

    Patching the Quest

--- ModMain.cs
+++ ModMain.cs
@@ -2,23 +2,45 @@
 using MelonLoader;
 using HarmonyLib;
 using PatchQuest;
+using UnityEngine;
 
 namespace MillimediaGames.NoHit
 {
     public class ModMain : MelonMod
     {
-        
+        public override void OnUpdate()
+        {
+            if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex == 0)
+                return;
+
+            if(Game.State == GameState.QUESTING)
+            {
+                // Forcing health values to be 1
+                Player.P1.Status.health = 1;
+                Player.P2.Status.health = 1;
+            }
+        }
     }
 
-    [HarmonyPatch(typeof(Scaling))]
-    [HarmonyPatch(nameof(Scaling.HealthCurve))]
+    [HarmonyPatch(typeof(PlayerStatus))]
+    [HarmonyPatch(nameof(PlayerStatus.MaxHealth))]
     [HarmonyPatch(MethodType.Getter)]
-    public class HealthCurvePatch
+    public class MaxHealthPatch
     {
-        public static void Postfix(ref float __result)
+        public static void Postfix(ref int __result)
         {
-            MelonLogger.Msg("HealthCurve get patched");
-            __result = 1f;
+            __result = 1;
+        }
+    }
+
+    [HarmonyPatch(typeof(PlayerStatus))]
+    [HarmonyPatch(nameof(PlayerStatus.CurrentHealth))]
+    [HarmonyPatch(MethodType.Getter)]
+    public class HealthPatch
+    {
+        public static void Postfix(ref int __result)
+        {
+            __result = 1;
         }
     }
 }
